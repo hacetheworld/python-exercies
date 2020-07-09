0980979e607be636627ba8524acf10f0232ffda5
Ajay <majay1638@gmail.com>
2020-07-09 23:14:31 +0530

    touple and sets

---
+++ dictonary/dict.py
@@ -0,0 +1,19 @@
+persons = {
+    "name": 'Ajay',
+    "age": 20,
+
+}
+
+# print(persons)
+# print(persons['age'])
+
+# for item in persons.items():
+#     # print(f"{key.upper()} : {persons[key]}")
+#     print(item)
+
+# print('address' in persons)
+# # persons.pop('name')
+# print(persons)
+
+touple = (1, 2)
+print(touple[0])
---
+++ list/list.py
@@ -0,0 +1,8 @@
+# listsOfName = ["Ajay", "Vijay", "Muskan"]
+# print(listsOfName.pop(1))
+# print(listsOfName)
+
+# comparishinos
+
+lists = [list(range(item, item+2)) for item in list(range(1, 10))]
+print(lists)
